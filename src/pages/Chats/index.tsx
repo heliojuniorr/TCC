4fddea797e8aa400dba3fc7302f17cdf8ad36cbe
Helio Junior <hcjr2000@gmail.com>
2022-11-14 19:45:05 -0300

    Melhorias

---
+++ src/pages/Chats/index.tsx
@@ -0,0 +1,60 @@
+import { useEffect } from 'react'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { useAuth } from '../../hooks/useAuth'
+import { ApiUserType } from '../../interfaces/types'
+import styles from '../Chats/styles.module.scss'
+
+export function Chats() {
+    const {updateUserValues, chatUsers} = useAuth()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const unsubscribe = () => {
+            updateUserValues()
+        }
+
+        return unsubscribe
+    }, [])
+
+    function Entity(props: ({value: ApiUserType})) {
+        const navigate = useNavigate()
+        function handleGetInTouch() {
+            navigate(`/chat/${props.value.id}`)
+        }
+
+        return (
+            <div className={styles.entityContainer}>
+                <div>
+                    <div>Nome: {props.value.name}</div>
+                    <div>Localidade: {props.value.location}</div>
+                    <div>Especialidade: {props.value.specialty}</div>
+                    <div>Área para empreender: {props.value.businessArea}</div>
+                    <div>Educação: {props.value.education}</div>
+                </div>
+
+                <button onClick={handleGetInTouch}>
+                    Entrar em contato
+                </button>
+            </div>
+        )
+    }
+
+    return (
+        <main className={styles.chatsContainer}>
+            <div className={styles.chatsUsersContainer}>
+                {
+                    chatUsers?.map((value) => {
+                        return (
+                            <Entity key={value.id} value={value}/>
+                        )
+                    })
+                }
+            </div>
+
+            <Button variant="primary" onClick={() => {navigate(-1)}}>
+                    Voltar
+            </Button>
+        </main>
+    )
+}
